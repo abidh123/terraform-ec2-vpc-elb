d9e1b864a2d27ecb57a8007382a3d5f6740cc37d
Abidh <sabidh@deloitte.com>
2022-11-17 20:40:57 +0530

    convert2223

--- ALB/alb-publicsecurity.tf
+++ ALB/alb-publicsecurity.tf
@@ -5,7 +5,7 @@ module "alb-public-security-group" {
   version = "4.13.1"
   name="alb_sg"
   description="opening ports for http and https in this group"
-  vpc_id= module.vpcnew1.vpc_id
+  vpc_id= module.vpcnew2.vpc_id
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules = ["http-80-tcp","https-443-tcp" ]
   egress_rules = [ "all-all" ]
--- ALB/provider.tf
+++ ALB/provider.tf
@@ -1,4 +1,4 @@
-terraform {
+/*terraform {
   required_version = ">= 0.14"
   required_providers {
     aws = {
@@ -17,7 +17,7 @@ terraform {
   }
   }
   
-
+*/
 
 provider "aws" {
   
